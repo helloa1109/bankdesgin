adf3ed8564f27a6729c4d0323ce25ca2f7111055
helloa1109 <helloa1109@naver.com>
2025-01-24 15:09:27 +0900

    test

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>test</h1>
+      <span>test</span>
+    </div>
+  );
+}
+
+export default App;
